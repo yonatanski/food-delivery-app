f80d10ffe9e3c894cd33cfd170dd384bae796a63
yonatanski <phpcodeyg@gmail.com>
2022-08-10 08:50:16 +0200

    footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react"
+import { UilFacebook, UilGithub, UilInstagram } from "@iconscout/react-unicons"
+import Logo from "../assets/Logo.png"
+import css from "../styles/Footer.module.css"
+import Image from "next/image"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={css.container}>
+      <span>ALL RIGHT RESERVED!</span>
+      <div className={css.socail}>
+        <UilFacebook size={45} />
+        <UilGithub size={45} />
+        <UilInstagram size={45} />
+      </div>
+      <div className={css.logo}>
+        <Image src={Logo} alt="logo of the navigation bar" width={50} height={50} />
+        <span>Foodie</span>
+      </div>
+    </div>
   )
 }
-
 export default Footer
